15ec3a9648cf8c3bb9063d7f63c7480e905ccffa
ffjt07 <110755216+ffjt07@users.noreply.github.com>
2022-08-21 16:36:37 -0600

    added functions when start button is clicked; timer and start quiz. also linked javascript to HTML

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,32 @@
 // Query Selectors of the sections in HTML
 var showQuiz = document.querySelector("#quiz")
-var timeLeft = document.querySelector("time-left")
+var timeLeft = document.querySelector("#time-left")
 var questionDisp = document.querySelector("#question")
-var answerList = document.querySelector("answer-list")
-var hideBegin = document.querySelector("begin")
+var answerList = document.querySelector("#answer-list")
+var hideBegin = document.querySelector(".begin")
+var startButton = document.querySelector("#start")
 
+// Global Variables
+var question = ""
+var timer;
+var timerCount;
 
+// Functions
+function startQuiz () {
+    timerCount = 75;
+    timeLeft.textContent = timerCount
+    hideBegin.setAttribute("style", "display: none");
+    startTimer();
+}
+
+function startTimer () {
+    timer = setInterval(function () {
+        timerCount--;
+        timeLeft.textContent = timerCount;
+        if (timerCount === 0) {
+            clearInterval(timer);
+        }
+    }, 1000)
+}
+
+startButton.addEventListener("click", startQuiz)
