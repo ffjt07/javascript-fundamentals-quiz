e0c2dcd88e99f9d5e952fdb8f8f397ff51a58316
ffjt07 <110755216+ffjt07@users.noreply.github.com>
2022-08-21 21:09:56 -0600

    added section to display list of high scores and section to input initials for high score recording

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,8 @@ var timeLeft = document.querySelector("#time-left")
 var questionDisp = document.querySelector("#question")
 var answerList = document.querySelector("#answer-list")
 var hideBegin = document.querySelector(".begin")
+var highScoreButton = document.querySelector(".score")
+var hideHs = document.querySelector("#high-score-display")
 var startButton = document.querySelector("#start")
 
 // Global Variables
@@ -29,4 +31,11 @@ function startTimer () {
     }, 1000)
 }
 
+function highScoreDisp() {
+    hideBegin.setAttribute("style", "display: none")
+    showQuiz.setAttribute("style", "display: none")
+    hideHs.setAttribute("style", "display: contents")
+}
+
 startButton.addEventListener("click", startQuiz)
+highScoreButton.addEventListener("click", highScoreDisp)
